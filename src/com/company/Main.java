81752ec0e59260fa4e28c467fbf70ddb8c052356
ANDE <ande@alphanetworks.be>
2016-11-30 11:57:58 +0100

    repair

---
+++ src/com/company/Main.java
@@ -0,0 +1,26 @@
+package com.company; /**
+ * Created by ANDE on 25/11/2016.
+ */
+
+import com.company.controller.Scenario;
+import com.company.view.Selector;
+
+public class Main{
+
+    public static void main(String[] args) {
+        Selector vs = new Selector();
+        Scenario cs = new Scenario();
+        /*Application opening*/
+        vs.openFrame();
+
+        /*Launch selected project*/
+        cs.initialize(vs.get_dataProject());
+
+        /*Execute selected sections*/
+        cs.execution(vs.get_dataSection(),true);
+
+        /*Application closing*/
+        cs.evacuation();
+
+    }
+}
